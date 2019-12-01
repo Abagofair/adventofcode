a07bdd3c4ed1bcc40f7f4dc3e17b86669d8b680b
Anders <anderskoch.airbag@gmail.com>
2019-12-01 10:45:30 +0100

    v8

--- AdventOfCode19/Utilities.cs
+++ AdventOfCode19/Utilities.cs
@@ -8,13 +8,10 @@ namespace AdventOfCode19
 		public static List<string> ReadFile(string fileName)
 		{
 			List<string> strings = new List<string>();
-			using (var streamReader = new StreamReader(File.Open(fileName, FileMode.Open, FileAccess.Read)))
-			{
-				while (!streamReader.EndOfStream)
-				{
-					strings.Add(streamReader.ReadLine());
-				}
-			}
+			using var streamReader = new StreamReader(File.Open(fileName, FileMode.Open, FileAccess.Read));
+
+			while (!streamReader.EndOfStream)
+				strings.Add(streamReader.ReadLine());
 
 			return strings;
 		}
